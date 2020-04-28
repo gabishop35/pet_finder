46625a31ca5c0af3308d7fac193d11e7cc2335ae
Geoff <gabishop35@gmail.com>
2020-04-28 16:31:56 -0400

    start building pet model

--- pet_finder/models.py
+++ pet_finder/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+STATE_CHOICES = []
+
+class Pet(models.Model):
+    img = 
+    name = models.CharField(max_length=60)
+    animal = models.Charfield(max_length=15)
+    gender = models.CharField(max_length=7)
+    breed = models.CharField(max_length=100, blank=True, null=True)
+    color = models.CharField(max_length=60, blank=True, null=True)
+    age = models.CharField(max_length=10, blank=True, null=True)
+    lost = models.DateField
+    address = models.CharField(max_length=75, blank=True, null=True)
+    city = models.CharField(max_length=50, blank=True, null=True)
+    state = models.CharField(max_length=2)
+    reward = models.CharField(max_length=15, blank=True, null=True)
+    description = models.TextField(max_length=400, blank=True, null=True)
+
