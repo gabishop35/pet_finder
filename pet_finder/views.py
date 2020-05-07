2ef097849d97a6b8f6a6467ab810258ced2f33ba
Geoff <gabishop35@gmail.com>
2020-05-07 19:00:46 -0400

    format pages to columns

--- pet_finder/views.py
+++ pet_finder/views.py
@@ -12,13 +12,14 @@ def homepage(request):
 def info(request):
     return render(request, 'core/info.html')
 
+@login_required
 def add_pet(request):
     if request.method =='POST':
         form = PetForm(request.POST, request.FILES)
         if form.is_valid():
-            catch = form.save(commit=False)
-            catch.user = request.user
-            catch.save()
+            pet = form.save(commit=False)
+            pet.user = request.user
+            pet.save()
             return redirect('profile-page')
     else:
         form = PetForm()
@@ -29,9 +30,9 @@ def edit_pet(request, pk):
     if request.method == 'POST':
         form = PetForm(request.POST, request.FILES, instance=pet)
         if form.is_valid():
-            catch = form.save(commit=False)
-            catch.user = request.user
-            catch.save()
+            pet = form.save(commit=False)
+            pet.user = request.user
+            pet.save()
             return redirect('profile-page')
     else: 
         form = PetForm(instance=pet)
